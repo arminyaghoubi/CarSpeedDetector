fef32f2c2f829e392dd79fd30c96be8b79dc8be8
ArminYaghoubi <ArminYaghoubi1@gmail.com>
2025-07-07 20:59:36 +0330

    Add Object Tracker for Every Element.

---
+++ core/object_tracker.py
@@ -0,0 +1,32 @@
+import numpy
+
+
+class ObjectTracker:
+    def __init__(self, distance_threshold=30):
+        self.next_id = 0
+        self.objects = {}
+        self.distance_threshold = distance_threshold
+
+    def update(self, detections):
+        updated_objects = {}
+        ids = set()
+
+        for cx, cy in detections:
+            matched_id = None
+            min_distance = float("inf")
+
+            for object_id, (px, py) in self.objects.items():
+                distance = numpy.hypot(cx - px, cy - py)
+                if distance < self.distance_threshold and object_id not in ids and distance < min_distance:
+                    matched_id = object_id
+                    min_distance = distance
+
+            if matched_id is not None:
+                updated_objects[matched_id] = (cx, cy)
+                ids.add(matched_id)
+            else:
+                updated_objects[self.next_id] = (cx, cy)
+                self.next_id += 1
+
+        self.objects=updated_objects
+        return self.objects
--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 import cv2
 from config import VIDEO_PATH
 from utils.drawing import draw_boxes
+from core.object_tracker import ObjectTracker
 from core.video_reader import VideoReader
 from core.object_detector import ObjectDetector
 
 
 def main():
+    tracker = ObjectTracker()
     target_classes = {2, 5, 7}  # car, bus, truck
     detector = ObjectDetector()
     class_names = detector.model.names
@@ -17,6 +19,19 @@ def main():
             break
 
         results = detector.detect(frame)
+
+        centers = []
+        for result in results:
+            for box in result.boxes:
+                if int(box.cls[0]) not in target_classes:
+                    continue
+                x1, y1, x2, y2 = box.xyxy[0]
+                cx = int((x1 + x2) / 2)
+                cy = int((y1 + y2) / 2)
+                centers.append((cx, cy))
+
+        tracked_objects = tracker.update(centers)
+
         draw_boxes(frame, results, class_names, target_classes)
 
         cv2.imshow("Traffic Video", frame)
